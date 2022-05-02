9b93490799798bcd2d3f5709c1ff77c62f04c3de
Bryan Prolong <smyllet.gamer@gmail.com>
2022-05-02 21:08:42 +0200

    Convert to typescript

--- routes/users.ts
+++ routes/users.ts
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+import Router from 'express';
+const router = Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
