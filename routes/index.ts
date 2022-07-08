5de8f04bdce5976d915d09b896d0a060329c677e
Bryan Prolong <smyllet.gamer@gmail.com>
2022-07-08 14:50:34 +0200

    [ADD] Measurement types list

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,15 @@
 import Router from 'express';
+import MeasurementTypeModel from "../model/MeasurementTypeModel";
 const router = Router();
 
 /* GET Hello World. */
 router.get('/', function(req, res, next) {
     res.send('Hello World');
 });
+
+/* get measurement types listing. */
+router.get('/measurements/types', function(req, res) {
+    res.json(MeasurementTypeModel.getAll())
+})
 
 module.exports = router;
